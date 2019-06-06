ddb3fc1daf2419f36d5d12e2c18a3975f72f65da
Kaustabh Ganguly <mymac@Kaustabhs-MacBook-Air.local>
2019-06-06 18:06:22 +0530

    test

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ app.use(bodyParser.json())
 
 // Index route
 app.get('/', function (req, res) {
-    res.send('Hello world, I am a chat bot')
+    res.send(<a href ='https://m.me/stabganpage'>I'm a chatbot</a>)
 })
 
 // for Facebook verification
