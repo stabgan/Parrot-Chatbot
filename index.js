de18a3037142289d614ff48f6152f4800231bb81
Kaustabh Ganguly <mymac@Kaustabhs-MacBook-Air.local>
2019-06-07 16:58:52 +0530

    rollback to working

--- index.js
+++ index.js
@@ -2,20 +2,20 @@ var express = require('express')
 var bodyParser = require('body-parser')
 var request = require('request')
 var app = express()
-
-app.set('port', (process.env.PORT || 6000))
-
+var weather = require('openweather-apis');
+var text2 = "none"
+ 
+weather.setLang('en');
+weather.setAPPID('6aa8478f4c0c55fe2ae9b1424cb7c900');
+app.set('port', (process.env.PORT || 5000))
 // Process application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({extended: false}))
-
 // Process application/json
 app.use(bodyParser.json())
-
 // Index route
 app.get('/', function (req, res) {
-    res.send('Hello world, I am a chat bot')
+    res.send('please visit https://m.me/stabganpage')
 })
-
 // for Facebook verification
 app.get('/webhook/', function (req, res) {
     if (req.query['hub.verify_token'] === 'Aha_Moment_Labs') {
@@ -23,15 +23,11 @@ app.get('/webhook/', function (req, res) {
     }
     res.send('Error, wrong token')
 })
-
 // Spin up the server
 app.listen(app.get('port'), function() {
     console.log('running on port', app.get('port'))
 })
-
-
 // API End Point - added by Stefan
-
 app.post('/webhook/', function(req, res) {
     messaging_events = req.body.entry[0].messaging
     for (i = 0; i < messaging_events.length; i++) {
@@ -43,7 +39,11 @@ app.post('/webhook/', function(req, res) {
                 sendGenericMessage(sender)
                 continue
             }
+
             sendTextMessage(sender, "parrot: " + text.substring(0, 200))
+            weatherX(sender , text)
+            //weatherX(sender , text)
+
         }
         if (event.postback) {
             text = JSON.stringify(event.postback)
@@ -53,11 +53,16 @@ app.post('/webhook/', function(req, res) {
     }
     res.sendStatus(200)
 })
-
-var token = "EAAHcUSVmZBjMBAMlHJZA05ide7qONeGZBsPY7DHlex4mUqIDbkDwoLkZCzeZBffspiIKUZARbLlwsvL9s2NciO0Y1Kx7v7z4OpmWgYBZC5fBwCKxRcvnNbfkZBqwNzUUzwI612ZCwYi5zaXedRdNuAxZBUYE8iac2c8gZCxwih5Hq8jiwdeqawjPvqU"
-
+var token = 'EAAHcUSVmZBjMBAGe6MRfzIJ9XfBck0irEXHg9qbZC6ifEuJ5rhY6TVeKbn1J5TRfHrTaYzujB4PkB7wJ5gQcVKaRp1KYPHfmCE2AOjMWrWhobZAUijqlCkw3D85cGS1B9HSIOCMRZBTLZBjqMvXjAxQ5cT6sxktA4Ftrv9ea3YRIDyIdf53GA'
 // function to echo back messages - added by Stefan
-
+function weatherX(sender , word_id){
+    weather.setCity(word_id);
+    weather.getDescription(function(err, desc){
+    console.log(desc);
+    text2 = desc;
+    });
+    sendTextMessage(sender , text2)
+}
 function sendTextMessage(sender, text) {
     messageData = {
         text:text
@@ -78,6 +83,7 @@ function sendTextMessage(sender, text) {
         }
     })
 }
+// Send an test message back as two cards.
 function sendGenericMessage(sender) {
     messageData = {
         "attachment": {
@@ -85,39 +91,55 @@ function sendGenericMessage(sender) {
             "payload": {
                 "template_type": "generic",
                 "elements": [{
-                    "title": "LinkedIn",
-                    "subtitle": "Connect with my owner !",
-                    "image_url": "https://cdn1.iconfinder.com/data/icons/logotypes/32/square-linkedin-512.png",
+                    "title": "Ai Chat Bot Communities",
+                    "subtitle": "Communities to Follow",
+                    "image_url": "http://1u88jj3r4db2x4txp44yqfj1.wpengine.netdna-cdn.com/wp-content/uploads/2016/04/chatbot-930x659.jpg",
                     "buttons": [{
                         "type": "web_url",
-                        "url": "https://linkedin.com/in/stabgan",
-                        "title": "Click to connect"
+                        "url": "https://www.facebook.com/groups/aichatbots/",
+                        "title": "FB Chatbot Group"
+                    }, {
+                        "type": "web_url",
+                        "url": "https://www.reddit.com/r/Chat_Bots/",
+                        "title": "Chatbots on Reddit"
+                    },{
+                        "type": "web_url",
+                        "url": "https://twitter.com/aichatbots",
+                        "title": "Chatbots on Twitter"
                     }],
                 }, {
-                    "title": "Github",
-                    "subtitle": "View my Owner's projects",
-                    "image_url": "https://i.vimeocdn.com/video/253024709.webp?mw=900&mh=508&q=70",
+                    "title": "Chatbots FAQ",
+                    "subtitle": "Aking the Deep Questions",
+                    "image_url": "https://tctechcrunch2011.files.wordpress.com/2016/04/facebook-chatbots.png?w=738",
                     "buttons": [{
-                        "type": "web_url",
-                        "url": "https://github.com/stabgan",
-                        "title": "Click to View"
+                        "type": "postback",
+                        "title": "What's the benefit?",
+                        "payload": "Chatbots make content interactive instead of static",
                     },{
                         "type": "postback",
-                        "title": "What are his interests ?",
-                        "payload": "He is interested in AI , ML , DL , Game Developement , UI/UX , Android , Backend etc ",
+                        "title": "What can Chatbots do",
+                        "payload": "One day Chatbots will control the Internet of Things! You will be able to control your homes temperature with a text",
+                    }, {
+                        "type": "postback",
+                        "title": "The Future",
+                        "payload": "Chatbots are fun! One day your BFF might be a Chatbot",
                     }],
                 },  {
-                    "title": "Portfolio",
-                    "subtitle": "View my Owner's portfolio website",
-                    "image_url": "https://images.unsplash.com/photo-1541422348463-9bc715520974?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&w=1000&q=80",
+                    "title": "Learning More",
+                    "subtitle": "Aking the Deep Questions",
+                    "image_url": "http://www.brandknewmag.com/wp-content/uploads/2015/12/cortana.jpg",
                     "buttons": [{
-                        "type": "web_url",
-                        "url": "https://stabgan.github.io",
-                        "title": "Click to View"
+                        "type": "postback",
+                        "title": "AIML",
+                        "payload": "Checkout Artificial Intelligence Mark Up Language. Its easier than you think!",
                     },{
-                        "type": "web_url",
-                        "url": "https://play.google.com/store/apps/details?id=com.whale.calculator&hl=en_IN",
-                        "title": "Click to download his app"
+                        "type": "postback",
+                        "title": "Machine Learning",
+                        "payload": "Use python to teach your maching in 16D space in 15min",
+                    }, {
+                        "type": "postback",
+                        "title": "Communities",
+                        "payload": "Online communities & Meetups are the best way to stay ahead of the curve!",
                     }],
                 }]  
             } 
@@ -139,4 +161,3 @@ function sendGenericMessage(sender) {
         }
     })
 }
-
